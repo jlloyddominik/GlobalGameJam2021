79c9a50e6df340f3cabc1658825b7c81e56269eb
Cowinatub <jstevadore@gmail.com>
2021-01-29 20:49:45 +1100

    I just did it all, whoops

---
+++ GGJ2021/Assets/Scripts/ColliderScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderScript : MonoBehaviour
+{
+	public Collider fakeCollider;
+	private ContactTracker contacts;
+	public LightCheckerScript[] lightCheckers;
+
+	// Start is called before the first frame update
+    void Start()
+    {
+		contacts = fakeCollider.GetComponent<ContactTracker>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		bool revealed = true;
+        foreach (LightCheckerScript checkerScript in lightCheckers) {
+			if (!checkerScript.revealed) {
+				revealed = false;
+			}
+		}
+
+		if (revealed) {
+			if (contacts.numberOfContacts == 0) {
+				fakeCollider.isTrigger = false;
+			}
+		} else {
+			fakeCollider.isTrigger = true;
+		}
+    }
+}
---
+++ GGJ2021/Assets/Scripts/lightColliders/LightSphere.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSphere : LightCollisionScript
+{
+	public override bool isInside(Vector3 point)
+	{
+		return !checkInShadow(point);
+	}
+}
