cdd92447b574b56f744a50833c5892c97592f423
Jesse Lloyd-Dominik <101663792@student.swin.edu.au>
2021-01-29 22:41:16 +1100

    Add: first pass of base, reveal, and disappear shaders

--- GGJ2021/Assets/Shaders/HLSL/AdditionalLights.hlsl
+++ GGJ2021/Assets/Shaders/HLSL/AdditionalLights.hlsl
@@ -5,19 +5,52 @@ void RevealLight_float(float3 Position, out float1 Value, out float3 Colour, out
 {
     float1 value = 0;
     float3 colour = 0;
-    float inLight;
+    float inLight = 0;
 #ifndef SHADERGRAPH_PREVIEW
     int pixelLightCount = GetAdditionalLightsCount();
     for (int i = 0; i < pixelLightCount; ++i)
     {
         Light light = GetAdditionalLight(i, Position);
         value += (light.distanceAttenuation * light.shadowAttenuation);
-        inLight += light.distanceAttenuation;
+        inLight += light.distanceAttenuation * light.shadowAttenuation;
         colour += light.color * (light.distanceAttenuation * light.shadowAttenuation);
     }
 #endif
     Value = value;
     Colour = colour;
     InLight = inLight > 0 ? 1 : 0;
+
 }
+
+// void DirectionalLight_float(float3 WorldPos, out float3 Colour, out float Shadow)
+// {
+// #if SHADOWS_SCREEN
+//     float4 clipPos = TransformWorldToHClip(WorldPos);
+//     float4 shadowCoord = ComputeScreenPos(clipPos);
+// #else
+//     float4 shadowCoord = TransformWorldToShadowCoord(WorldPos);
+// #endif
+//     Light mainLight = GetMainLight(shadowCoord);
+//     Colour = mainLight.color * mainLight.shadowAttenuation;
+//     Shadow = mainLight.shadowAttenuation;
+// }
+
+void MainLight_float(float3 WorldPos, out float3 Colour, out float Shadow)
+{
+#if SHADERGRAPH_PREVIEW
+    Colour = 1;
+    Shadow = 1;
+#else
+#if SHADOWS_SCREEN
+    float4 clipPos = TransformWorldToHClip(WorldPos);
+    float4 shadowCoord = ComputeScreenPos(clipPos);
+#else
+    float4 shadowCoord = TransformWorldToShadowCoord(WorldPos);
+#endif
+    Light mainLight = GetMainLight(shadowCoord);
+    Colour = mainLight.color;
+    Shadow = mainLight.shadowAttenuation;
+#endif
+}
+
 #endif
