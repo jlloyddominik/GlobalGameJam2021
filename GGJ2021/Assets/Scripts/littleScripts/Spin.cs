f4b03647b4eda291391b07d7feabdab9a7bf83a4
Cowinatub <jstevadore@gmail.com>
2021-01-31 00:47:36 +1100

    Corridor of Swingin Lamps

---
+++ GGJ2021/Assets/Scripts/littleScripts/Spin.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour
+{
+    public Vector3 spinSpeed;
+	Rigidbody rb;
+
+	// Start is called before the first frame update
+    void Start()
+    {
+		rb = GetComponent<Rigidbody>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		rb.MoveRotation(transform.rotation * Quaternion.Euler(Time.deltaTime * spinSpeed));
+    }
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		collision.collider.transform.SetParent(transform);
+	}
+}
