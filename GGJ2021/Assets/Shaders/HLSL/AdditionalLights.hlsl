b40ad12ce5c4babd4a2636d7dcadd819fd338df3
Jesse Lloyd-Dominik <101663792@student.swin.edu.au>
2021-01-31 00:06:19 +1100

    Add: wall and floor textures, spot effect materials

--- GGJ2021/Assets/Shaders/HLSL/AdditionalLights.hlsl
+++ GGJ2021/Assets/Shaders/HLSL/AdditionalLights.hlsl
@@ -1,6 +1,23 @@
 #ifndef CUSTOM_LIGHTING_INCLUDED
 #define CUSTOM_LIGHTING_INCLUDED
 
+void RevealSingle_float(float3 Position, int index, out float1 Value, out float3 Colour, out float1 InLight)
+{
+    float1 value = 0;
+    float3 colour = 0;
+    float inLight = 0;
+#ifndef SHADERGRAPH_PREVIEW
+    Light light = GetAdditionalLight(index, Position);
+    value = (light.distanceAttenuation * light.shadowAttenuation);
+    inLight = light.distanceAttenuation * light.shadowAttenuation;
+    colour = light.color * (light.distanceAttenuation * light.shadowAttenuation);
+#endif
+    Value = value;
+    Colour = colour;
+    InLight = inLight > 0 ? 1 : 0;
+
+}
+
 void RevealLight_float(float3 Position, out float1 Value, out float3 Colour, out float1 InLight)
 {
     float1 value = 0;
