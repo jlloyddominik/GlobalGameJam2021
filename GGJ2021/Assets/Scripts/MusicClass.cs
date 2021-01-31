2a33d926f633511e06aab526735ff11cf61204f8
Stewart <101082708@student.swin.edu.au>
2021-01-31 15:01:28 +1100

    Music Player

---
+++ GGJ2021/Assets/Scripts/MusicClass.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//https://answers.unity.com/questions/1260393/make-music-continue-playing-through-scenes.html
+public class MusicClass : MonoBehaviour
+{
+    private AudioSource _audioSource;
+    private void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+        _audioSource = GetComponent<AudioSource>();
+        PlayMusic();
+    }
+
+    public void PlayMusic()
+    {
+        if (_audioSource.isPlaying) return;
+        _audioSource.Play();
+    }
+
+    public void StopMusic()
+    {
+        _audioSource.Stop();
+    }
+}
